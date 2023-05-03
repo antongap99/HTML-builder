5cda0c996f971ff88e9fdb3befa146bebf6da257
Anton <anton.gap99@gmail.com>
2023-05-03 15:43:53 +0300

    feat: complete tast 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require("fs/promises");
+const path = require("path");
+const copied = path.join(__dirname, 'files-copy')
+const dir =  path.join(__dirname, 'files');
 
+const copyDir = async () => {
+  try {
+    await fs.mkdir(copied, {recursive: true});
+    const data = await fs.readdir(dir, { withFileTypes: true });
+    for await (const file of data) {
+      fs.copyFile(path.join(dir, file.name), path.join(copied, file.name));
+    }
+  } catch (error) {
+    console.log('error: ', error);
+  }
+};
+copyDir();
